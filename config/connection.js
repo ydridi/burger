0df693872b978c49f1d6714f6d0aa64099b86d00
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-05-25 14:52:00 -0400

    adding new db to my connection js needs for the deployment heroku

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,18 @@
 var mysql = require("mysql");
 var connection;
+if (process.env.JAWSDB_URL) 
+{
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else 
+{
+    connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+    });
+};
 
 if (process.env.JAWSDB_URL) 
 {
