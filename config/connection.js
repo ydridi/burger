286d8e16c5eb71e6ad5a70e9657848092dd727f0
yassine dridi <58660795+ydridi@users.noreply.github.com>
2020-05-25 16:08:47 -0400

    some changes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,6 @@
 var mysql = require("mysql");
 var connection;
-if (process.env.JAWSDB_URL) 
-{
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-}
-else 
-{
-    connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
-    });
-};
+
 
 if (process.env.JAWSDB_URL) 
 {
